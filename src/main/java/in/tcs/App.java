62062ea9562743da1fcd95effcea556418c45a73
shreyas04100 <shgk23cs@cmrit.ac.in>
2026-04-16 08:40:19 +0530

    Add files via upload

---
+++ src/main/java/in/tcs/App.java
@@ -0,0 +1,14 @@
+package in.tcs;
+
+/**
+ * Hello world!
+ */
+public class App {
+   
+    public static void main(String[] args) {
+        int p=4,q=2,quot;
+        quot=p/q;
+        System.out.println("Quot"+quot);
+
+    }
+}
